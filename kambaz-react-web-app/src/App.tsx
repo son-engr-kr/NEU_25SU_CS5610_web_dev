660db8782bfa5580c215cc03537eed93ac35fd6a
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-08 19:11:43 -0400

    A1 - router (~1.3.9)

--- kambaz-react-web-app/src/App.tsx
+++ kambaz-react-web-app/src/App.tsx
@@ -1,8 +1,13 @@
-// import React from "react";
 import Labs from "./Labs";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
-    <div>
-      <Labs />
-    </div>
-);}
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Labs" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+        </Routes>
+      </div>
+    </HashRouter>);
+}
