c623c499ad9afb31c96d7eb17f17fe4a11f1ff64
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-08 20:33:00 -0400

    A1 -1.4.5

--- kambaz-react-web-app/src/Kambaz/Account/Signin.tsx
+++ kambaz-react-web-app/src/Kambaz/Account/Signin.tsx
@@ -3,9 +3,14 @@ export default function Signin() {
   return (
     <div id="wd-signin-screen">
       <h3>Sign in</h3>
-      <input placeholder="username" className="wd-username" /> <br />
-      <input placeholder="password" type="password" className="wd-password" /> <br />
-      <Link  to="/Kambaz/Account/Profile" id="wd-signin-btn"> Sign in </Link> <br />
-      <Link  to="/Kambaz/Account/Signup"  id="wd-signup-link">Sign up</Link>
+      <input className="wd-username" placeholder="username" /> <br />
+      <input className="wd-password" placeholder="password" type="password" /> <br />
+      <Link id="wd-signin-btn" to="/Kambaz/Dashboard">
+        Sign in
+      </Link>
+      <br />
+      <Link id="wd-signup-link" to="/Kambaz/Account/Signup">
+        Sign up
+      </Link>
     </div>
 );}
