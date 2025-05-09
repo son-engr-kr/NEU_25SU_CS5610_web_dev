82d492efa9c6bd2917ac4ebeeb408e503d2fbf03
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-08 21:37:57 -0400

    A1 1.4.8

--- kambaz-react-web-app/src/Kambaz/Courses/index.tsx
+++ kambaz-react-web-app/src/Kambaz/Courses/index.tsx
@@ -1,4 +1,5 @@
 import Assignments from "./Assignments";
+import AssignmentEditor from "./Assignments/Editor";
 import CourseNavigation from "./Navigation";
 import { Navigate, Route, Routes } from "react-router";
 import Modules from "./Modules";
@@ -19,7 +20,7 @@ export default function Courses() {
               <Route path="Home" element={<Home />} />
               <Route path="Modules" element={<Modules />} />
               <Route path="Assignments" element={<Assignments />} />
-              <Route path="Assignments/:aid" element={<h2>Assignment Editor</h2>} />
+              <Route path="Assignments/:aid" element={<AssignmentEditor />} />
               <Route path="People" element={<h2>People</h2>} />
             </Routes>
           </td>
