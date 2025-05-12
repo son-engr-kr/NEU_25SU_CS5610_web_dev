9e719d63c323bfe5e5c49b6d3b0269761656b576
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-12 14:21:29 -0400

    2.1.5

--- kambaz-react-web-app/src/Labs/Lab2/index.tsx
+++ kambaz-react-web-app/src/Labs/Lab2/index.tsx
@@ -30,6 +30,46 @@ export default function Lab2() {
 
       </div>
 
+      {/* 2.1.5 */}
+      {/* this is parent element with selector
+      .wd-selector-1
+
+      .wd-selector-2 is a direct child of
+      .wd-selector-1
+
+      .wd-selector-3 is  a direct child of
+      .wd-selector-2 and a descendant of
+      .wd-selector-1
+
+
+      .wd-selector-4 is a direct child of
+      .wd-selector-3 and is a descendant of
+      .wd-selector-2 and is a descendant of
+      .wd-selector-1
+
+      */}
+
+      <div id="wd-css-document-structure">
+        <div className="wd-selector-1">
+          <h3>Document structure selectors</h3>
+          <div className="wd-selector-2">
+            Selectors can be combined to refer elements in particular
+            places in the document
+            <p className="wd-selector-3">
+              This paragraph's red background is referenced as
+              <br />
+              .selector-2 .selector3<br />
+              meaning the descendant of some ancestor.<br />
+              <span className="wd-selector-4">
+                Whereas this span is a direct child of its parent
+              </span><br />
+              You can combine these relationships to create specific
+              styles depending on the document structure
+            </p>
+          </div>
+        </div>
+      </div>
+
     </div>
 
 
