e9b7891212012e2cd4bd9c01262e389ecc5481df
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-12 14:55:46 -0400

    2.1.10

--- kambaz-react-web-app/src/Labs/Lab2/index.tsx
+++ kambaz-react-web-app/src/Labs/Lab2/index.tsx
@@ -69,7 +69,7 @@ export default function Lab2() {
           </div>
         </div>
       </div>
-
+      {/* 2.1.7 */}
       <div id="wd-css-colors">
         <h2>Colors</h2>
         <h3 className="wd-fg-color-blue">Foreground color</h3>
@@ -102,6 +102,54 @@ export default function Lab2() {
      wd-border-dashed">
           Dashed thin blue border
         </p>
+      </div>
+
+      {/* 2.1.10 */}
+      <div id="wd-css-paddings">
+        <h2>Padding</h2>
+        <div className="wd-padded-top-left wd-border-fat 
+      wd-border-red wd-border-solid 
+      wd-bg-color-yellow">
+          Padded top left
+        </div>
+        <div className="wd-padded-bottom-right wd-border-fat 
+      wd-border-blue wd-border-solid 
+      wd-bg-color-yellow">
+          Padded bottom right
+        </div>
+        <div className="wd-padding-fat wd-border-fat 
+      wd-border-yellow wd-border-solid 
+      wd-bg-color-blue wd-fg-color-white">
+          Padded all around  </div>
+      </div>
+
+      <div id="wd-css-margins">
+        <h2>Margins</h2>
+        <div
+          className="wd-margin-bottom 
+      wd-padded-top-left 
+      wd-border-fat wd-border-red 
+      wd-border-solid 
+      wd-bg-color-yellow">
+          Margin bottom
+        </div>
+        <div
+          className="wd-margin-right-left 
+      wd-padded-bottom-right 
+      wd-border-fat wd-border-blue 
+      wd-border-solid 
+      wd-bg-color-yellow">
+          Margin left right
+        </div>
+        <div
+          className="wd-margin-all-around 
+      wd-padding-fat wd-border-fat 
+      wd-border-yellow 
+      wd-border-solid 
+      wd-bg-color-blue 
+      wd-fg-color-white">
+          Margin all around
+        </div>
       </div>
 
 
