cf8f36b759514a36fd33374917ffe90f2cbb1c6f
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-12 19:07:51 -0400

    2.3.4 Implementing Responsive HTML Content with React Bootstrap Grids

--- kambaz-react-web-app/src/Labs/Lab2/BootstrapGrids.tsx
+++ kambaz-react-web-app/src/Labs/Lab2/BootstrapGrids.tsx
@@ -34,6 +34,75 @@ export default function Base() {
                         <h3>Sidebar</h3>
                     </Col>  </Row>  </div>
 
+
+            {/* 2.3.4 */}
+            {/* Check if layout changes when browser window is resized */}
+            <div id="wd-bs-responsive-grids">
+                <h2>Responsive grid system</h2>
+                <Row>
+                    <Col xs={12} md={6} xl={3}
+                        className="bg-warning">
+                        <h3>Column A</h3>
+                    </Col>
+                    <Col xs={12} md={6} xl={3}
+                        className="bg-primary text-white">
+                        <h3>Column B</h3>
+                    </Col>
+                    <Col xs={12} md={6} xl={3}
+                        className="bg-danger text-white">
+                        <h3>Column C</h3>
+                    </Col>
+                    <Col xs={12} md={6} xl={3}
+                        className="bg-success text-white">
+                        <h3>Column D</h3>
+                    </Col>
+                </Row>
+            </div>
+
+
+            <div id="wd-bs-responsive-dramatic">
+                <h2>Responsive grid system</h2>
+                <Row>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-warning">
+                        <h4>1</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-primary text-white">
+                        <h4>2</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-danger text-white">
+                        <h4>3</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-success text-white">
+                        <h4>4</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-warning">
+                        <h4>5</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-primary text-white">
+                        <h4>6</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-danger text-white">
+                        <h4>7</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-success text-white">
+                        <h4>8</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-warning">
+                        <h4>9</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-primary text-white">
+                        <h4>10</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-danger text-white">
+                        <h4>11</h4></Col>
+                    <Col xs={12} sm={6} md={4} lg={3} xl={2} xxl={1}
+                        className="bg-success text-white">
+                        <h4>12</h4></Col>
+                </Row>
+            </div>
+
+
         </div>
     )
 }
