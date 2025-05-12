f4250070fb2b45f625929761113b38b51a0804a0
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-12 19:47:44 -0400

    2.3.7 Implementing Responsive Tables with React Bootstrap

--- kambaz-react-web-app/src/Labs/Lab2/BootstrapTables.tsx
+++ kambaz-react-web-app/src/Labs/Lab2/BootstrapTables.tsx
@@ -21,6 +21,25 @@ export default function Base() {
                 </Table>
             </div>
 
+            <div id="wd-css-responsive-tables">
+                <h2>Responsive tables</h2>
+                <Table responsive>
+                    <thead>
+                        <tr><th>Very</th><th>long</th><th>set</th><th>of</th><th>columns</th>
+                            <th>Very</th><th>long</th><th>set</th><th>of</th><th>columns</th>
+                            <th>Very</th><th>long</th><th>set</th><th>of</th><th>columns</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Very</td><td>long</td><td>set</td><td>of</td><td>columns</td>
+                            <td>Very</td><td>long</td><td>set</td><td>of</td><td>columns</td>
+                            <td>Very</td><td>long</td><td>set</td><td>of</td><td>columns</td>
+                        </tr>
+                    </tbody>
+                </Table>
+            </div>
+
         </div>
     )
 }
