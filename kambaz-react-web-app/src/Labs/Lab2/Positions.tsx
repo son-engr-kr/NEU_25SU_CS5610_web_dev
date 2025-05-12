dae4efcc2673fb40ad4372fcdb7da826ade071d4
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-12 16:13:04 -0400

    2.1.15 Styling an HTML Tag's Fixed Position with CSS

--- kambaz-react-web-app/src/Labs/Lab2/Positions.tsx
+++ kambaz-react-web-app/src/Labs/Lab2/Positions.tsx
@@ -36,6 +36,18 @@ export default function Base() {
                 </div><br /><br /><br /><br /><br /><br /><br />
             </div>
 
+            {/* 2.1.15 */}
+            <div id="wd-css-position-fixed">
+                <h2>Fixed position</h2>
+                Checkout the blue square that says "Fixed position" stuck all the way on the right and half way down the page. It doesn't scroll with the rest of the page. Its position is "Fixed".
+                <div className="wd-pos-fixed 
+    wd-dimension-square wd-bg-color-blue 
+    wd-fg-color-white">
+                    Fixed position
+                </div>
+            </div>
+
+
 
         </div>
     )
