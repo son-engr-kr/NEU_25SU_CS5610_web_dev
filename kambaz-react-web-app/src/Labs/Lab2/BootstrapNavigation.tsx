fb678fe3c23ec85fec9a2282fca87e842664783d
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-12 22:12:30 -0400

    2.3.18 Styling Navigation with React Bootstrap Cards

--- kambaz-react-web-app/src/Labs/Lab2/BootstrapNavigation.tsx
+++ kambaz-react-web-app/src/Labs/Lab2/BootstrapNavigation.tsx
@@ -1,4 +1,4 @@
-import { Nav } from "react-bootstrap";
+import { Button, Card, Nav } from "react-bootstrap";
 
 export default function Base() {
     return (
@@ -21,6 +21,23 @@ export default function Base() {
                     </Nav.Item>
                 </Nav>
             </div>
+
+            <div id="wd-css-navigating-with-cards">
+                <h2>
+                    Cards
+                </h2>
+                <Card style={{ width: "18rem" }}>
+                    <Card.Img variant="top" src="images/stacked.jpg" />
+                    <Card.Body>
+                        <Card.Title>Stacking Starship</Card.Title>
+                        <Card.Text>
+                            Stacking the most powerful rocket in history. Mars or bust!
+                        </Card.Text>
+                        <Button variant="primary">Boldly Go</Button>
+                    </Card.Body>
+                </Card>
+            </div>
+
 
 
         </div>
