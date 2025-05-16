c19e0d7e15b9dee2b50c62b8ea14e828799ffeb9
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-15 20:43:30 -0400

    2.4.1 Styling the Kambaz Navigation Sidebar - 1

--- kambaz-react-web-app/src/Kambaz/Navigation.tsx
+++ kambaz-react-web-app/src/Kambaz/Navigation.tsx
@@ -1,14 +1,30 @@
+import { AiOutlineDashboard } from "react-icons/ai";
+import { LiaBookSolid } from "react-icons/lia";
+import { FaRegCircleUser } from "react-icons/fa6";
+import { ListGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 export default function KambazNavigation() {
   return (
-    <div id="wd-kambaz-navigation">
-      <a href="https://www.northeastern.edu/" id="wd-neu-link" target="_blank">Northeastern</a><br/>
-      <Link to="/Kambaz/Account" id="wd-account-link">Account</Link><br/>
-      <Link to="/Kambaz/Dashboard" id="wd-dashboard-link">Dashboard</Link><br/>
-      {/* DONE(A1): This is not typo maybe. Course should take us to Dashboard */}
-      <Link to="/Kambaz/Dashboard" id="wd-course-link">Courses</Link><br/>
-      <Link to="/Kambaz/Calendar" id="wd-calendar-link">Calendar</Link><br/>
-      <Link to="/Kambaz/Inbox" id="wd-inbox-link">Inbox</Link><br/>
-      <Link to="/Labs" id="wd-labs-link">Labs</Link><br/>
-    </div>
-);}
+    <ListGroup id="wd-kambaz-navigation" style={{ width: 120 }}
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
+      <ListGroup.Item id="wd-neu-link" target="_blank" action
+        href="https://www.northeastern.edu/"
+        className="bg-black border-0 text-center">
+        <img src="/images/NEU.png" width="75px" /></ListGroup.Item><br />
+      <ListGroup.Item to="/Kambaz/Account" as={Link}
+        className="text-center border-0 bg-black text-white">
+        <FaRegCircleUser className="fs-1 text text-white" /><br />
+        Account </ListGroup.Item>
+      <ListGroup.Item to="/Kambaz/Dashboard" as={Link}
+        className="text-center border-0
+                  bg-white text-danger">
+        <AiOutlineDashboard className="fs-1 text-danger" /><br />
+        Dashboard </ListGroup.Item>
+      <ListGroup.Item to="/Kambaz/Dashboard" as={Link}
+        className="text-white
+                  bg-black text-center border-0">
+        <LiaBookSolid className="fs-1 text-danger" /><br />
+        Courses </ListGroup.Item>
+      {/* complete styling the rest of the links */}
+    </ListGroup>);
+}
