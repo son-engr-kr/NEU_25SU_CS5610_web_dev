ca5ec89db95289be4674453ea20231868230b5f2
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-15 21:57:10 -0400

    2.4.4 Styling the Modules Screen  - Follow

--- kambaz-react-web-app/src/Kambaz/Courses/Modules/index.tsx
+++ kambaz-react-web-app/src/Kambaz/Courses/Modules/index.tsx
@@ -1,79 +1,81 @@
+import { BsGripVertical } from 'react-icons/bs';
+import ModulesControls from './ModulesControls';
+import { ListGroup } from 'react-bootstrap';
+import LessonControlButtons from './LessonControlButtons';
+import ModuleControlButtons from './ModuleControlButtons';
+
 export default function Modules() {
-    return (
-      <div>
-        {/* TODO(A1):Implement Collapse All button, View Progress button, etc. */}
+  return (
+    <div>
+      <ModulesControls />
 
-        {/*  Collapse All button */}
-        <button id="wd-collapse-all" onClick={() => alert("Collapse All functionality not yet implemented")}>
-          Collapse All
-        </button>
-
-        {/*  View Progress button */}
-        <button id="wd-view-progress" onClick={() => alert("View Progress functionality not yet implemented")}>
-          View Progress
-        </button>
-
-        {/*  Publish All dropdown */}
-        <select id="wd-publish-all" onChange={() => alert("Publish All functionality not yet implemented")}>
-          <option value="">Publish All</option>
-          <option value="publish-now">Publish Now</option>
-          <option value="schedule-publish">Schedule Publish</option>
-        </select>
-
-        {/*  +Module button */}
-        <button id="wd-add-module" onClick={() => alert("+Module functionality not yet implemented")}>
-          +Module
-        </button>
-
-        <ul id="wd-modules">
-          <li className="wd-module">
-            <div className="wd-title">Week 1, Lecture 1 - Course Introduction, Syllabus, Agenda</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
-                <ul className="wd-content">
-                  <li className="wd-content-item">Introduction to the course</li>
-                  <li className="wd-content-item">Learn what is Web Development</li>
-                </ul>
-              </li>
-              <li className="wd-lesson">
-                <span className="wd-title">READING</span>
-                <ul className="wd-content">
-                  <li className="wd-content-item">Full Stack Developer - Chapter 1 - Introduction</li>
-                  <li className="wd-content-item">Full Stack Developer - Chapter 2 - Creating Us</li>
-                </ul>
-              </li>
-              <li className="wd-lesson">
-                <span className="wd-title">SLIDES</span>
-                <ul className="wd-content">
-                  <li className="wd-content-item">Introduction to Web Development</li>
-                  <li className="wd-content-item">Creating an HTTP server with Node.js</li>
-                  <li className="wd-content-item">Creating a React Application</li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-          <li className="wd-module">
-            <div className="wd-title">Week 1, Lecture 2 - Formatting User Interfaces with HTML</div>
-            <ul className="wd-lessons">
-              <li className="wd-lesson">
-                <span className="wd-title">LEARNING OBJECTIVES</span>
-                <ul className="wd-content">
-                  <li className="wd-content-item">Learn how to create user interfaces with HTML</li>
-                  <li className="wd-content-item">Deploy the assignment to Netlify</li>
-                </ul>
-              </li>
-              <li className="wd-lesson">
-                <span className="wd-title">SLIDES</span>
-                <ul className="wd-content">
-                  <li className="wd-content-item">Introduction to HTML and the DOM</li>
-                  <li className="wd-content-item">Formatting Web content with Headings and</li>
-                  <li className="wd-content-item">Formatting content with Lists and Tables</li>
-                </ul>
-              </li>
-            </ul>
-          </li>
-        </ul>
-      </div>
-  );}
-  
+      <ListGroup className="rounded-0" id="wd-modules">
+        <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            <BsGripVertical className="me-2 fs-3" /> Week 1 <ModuleControlButtons />
+          </div>
+          <ListGroup className="wd-lessons rounded-0">
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              <BsGripVertical className="me-2 fs-3" /> LEARNING OBJECTIVES <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Introduction to the course <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Learn what is Web Development <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              READING <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Full Stack Developer - Chapter 1 - Introduction <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Full Stack Developer - Chapter 2 - Creating Us <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              SLIDES <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Introduction to Web Development <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Creating an HTTP server with Node.js <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Creating a React Application <LessonControlButtons />
+            </ListGroup.Item>
+          </ListGroup>
+        </ListGroup.Item>
+        <ListGroup.Item className="wd-module p-0 mb-5 fs-5 border-gray">
+          <div className="wd-title p-3 ps-2 bg-secondary">
+            Week 1, Lecture 2 - Formatting User Interfaces with HTML <ModuleControlButtons />
+          </div>
+          <ListGroup className="wd-lessons rounded-0">
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              LEARNING OBJECTIVES <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Learn how to create user interfaces with HTML <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Deploy the assignment to Netlify <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              SLIDES <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Introduction to HTML and the DOM <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Formatting Web content with Headings and <LessonControlButtons />
+            </ListGroup.Item>
+            <ListGroup.Item className="wd-lesson p-3 ps-1">
+              Formatting content with Lists and Tables <LessonControlButtons />
+            </ListGroup.Item>
+          </ListGroup>
+        </ListGroup.Item>
+      </ListGroup>
+    </div>
+  );
+}
