61bc02f3ea28664a39ca830eb3e5a599856f1222
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-15 20:56:05 -0400

    2.4.1 - import "./styles.css";

--- kambaz-react-web-app/src/Kambaz/index.tsx
+++ kambaz-react-web-app/src/Kambaz/index.tsx
@@ -4,6 +4,7 @@ import Account from "./Account";
 import Dashboard from "./Dashboard";
 import KambazNavigation from "./Navigation";
 import Courses from "./Courses";
+import "./styles.css";
 export default function Kambaz() {
   return (
     <div id="wd-kambaz">
