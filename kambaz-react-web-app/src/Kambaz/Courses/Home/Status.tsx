1de812393408ef91c2bc6b04d25bfd980b3d2f06
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-16 21:23:01 -0400

    Restyle the Course Status

--- kambaz-react-web-app/src/Kambaz/Courses/Home/Status.tsx
+++ kambaz-react-web-app/src/Kambaz/Courses/Home/Status.tsx
@@ -1,36 +1,45 @@
+import { Button } from "react-bootstrap";
+
 export default function CourseStatus() {
   // DONE(A2): 1.4.6 Restyle the Course Status
-    return (
-      <div id="wd-course-status">
-        <h2>Course Status</h2>
-        <div>
-          <button>Unpublish</button>
+  return (
+    <div id="wd-course-status">
+      <h2>Course Status</h2>
+      <div className="d-grid gap-2">
+        <div className="d-flex gap-2">
+          <Button variant="secondary" size="lg" className="flex-grow-1">
+            Unpublish
+          </Button>
+          <Button variant="secondary" size="lg" className="flex-grow-1">
+            Publish
+          </Button>
         </div>
-        <div>
-          <button>Publish</button>
-        </div>
-        {/* DONE(A1): Complete on your own 1.4.6 */}
-        <div>
-          <button>Import Existing Content</button>
-        </div>
-        <div>
-          <button>Import from Commons</button>
-        </div>
-        <div>
-          <button>Choose Home Page</button>
-        </div>
-        <div>
-          <button>View Course Stream</button>
-        </div>
-        <div>
-          <button>New Announcement</button>
-        </div>
-        <div>
-          <button>New Analytics</button>
-        </div>
-        <div>
-          <button>View Course Notifications</button>
+        <div className="mt-4">
+          <div className="d-grid gap-2">
+            <Button variant="secondary" size="lg" className="w-100">
+              Import Existing Content
+            </Button>
+            <Button variant="secondary" size="lg" className="w-100">
+              Import from Commons
+            </Button>
+            <Button variant="secondary" size="lg" className="w-100">
+              Choose Home Page
+            </Button>
+            <Button variant="secondary" size="lg" className="w-100">
+              View Course Stream
+            </Button>
+            <Button variant="secondary" size="lg" className="w-100">
+              New Announcement
+            </Button>
+            <Button variant="secondary" size="lg" className="w-100">
+              New Analytics
+            </Button>
+            <Button variant="secondary" size="lg" className="w-100">
+              View Course Notifications
+            </Button>
+          </div>
         </div>
       </div>
-    );
+    </div>
+  );
 }
