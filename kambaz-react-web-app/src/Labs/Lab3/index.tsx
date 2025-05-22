f18221befd424871ea3b595d19cafc01c2d44b71
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-22 13:12:29 -0400

    3.2.1 Variables and Constants

--- kambaz-react-web-app/src/Labs/Lab3/index.tsx
+++ kambaz-react-web-app/src/Labs/Lab3/index.tsx
@@ -1,7 +1,10 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+
 export default function Lab3() {
-  return (
+  return(
     <div id="wd-lab3">
       <h3>Lab 3</h3>
+      <VariablesAndConstants/>
     </div>
   );
 }
