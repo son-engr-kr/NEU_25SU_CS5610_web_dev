56ebff8ae85563c994b7a1029edb52e992e1d2cf
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-22 13:14:28 -0400

    3.2.3 Boolean Variables

--- kambaz-react-web-app/src/Labs/Lab3/index.tsx
+++ kambaz-react-web-app/src/Labs/Lab3/index.tsx
@@ -1,11 +1,13 @@
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
 export default function Lab3() {
   return(
     <div id="wd-lab3">
       <h3>Lab 3</h3>
       <VariablesAndConstants/>
       <VariableTypes/>
+      <BooleanVariables/>
     </div>
   );
 }
