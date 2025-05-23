8f0219ffd3e934eec4509f8e5970ac355605fe99
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-23 16:41:55 -0400

    3.4 JavaScript Data Structures

--- kambaz-react-web-app/src/Labs/Lab3/index.tsx
+++ kambaz-react-web-app/src/Labs/Lab3/index.tsx
@@ -9,6 +9,7 @@ import LegacyFunctions from "./LegacyFunctions";
 import ArrowFunctions from "./ArrowFunctions";
 import ImpliedReturn from "./ImpliedReturn";
 import TemplateLiterals from "./TemplateLiterals";
+import SimpleArrays from "./SimpleArrays";
 export default function Lab3() {
   return(
     <div id="wd-lab3">
@@ -24,6 +25,7 @@ export default function Lab3() {
       <ArrowFunctions/>
       <ImpliedReturn/>
       <TemplateLiterals/>
+      <SimpleArrays/>
     </div>
   );
 }
