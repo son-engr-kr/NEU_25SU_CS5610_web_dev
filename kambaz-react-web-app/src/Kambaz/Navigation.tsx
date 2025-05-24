c35e0bb4ec6461744c67ab6736f26c007da620de
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-24 01:07:46 -0400

    3.8.1 Data Driven Kambaz Navigation

--- kambaz-react-web-app/src/Kambaz/Navigation.tsx
+++ kambaz-react-web-app/src/Kambaz/Navigation.tsx
@@ -1,73 +1,44 @@
-import { AiOutlineDashboard, AiOutlineCalendar, AiOutlineInbox } from "react-icons/ai";
-import { LiaBookSolid } from "react-icons/lia";
-import { FaRegCircleUser } from "react-icons/fa6";
-import { FaFlask } from "react-icons/fa";
+import { AiOutlineDashboard } from "react-icons/ai";
+import { IoCalendarOutline } from "react-icons/io5";
+import { LiaBookSolid, LiaCogSolid } from "react-icons/lia";
+import { FaInbox, FaRegCircleUser } from "react-icons/fa6";
 import { ListGroup } from "react-bootstrap";
 import { Link, useLocation } from "react-router-dom";
 
 export default function KambazNavigation() {
-  const location = useLocation();
-  {/* DONE(A2): 2.4.1, Style the navigation bar */}
+  const { pathname } = useLocation();
+  const links = [
+    { label: "Dashboard", path: "/Kambaz/Dashboard", icon: AiOutlineDashboard },
+    { label: "Courses", path: "/Kambaz/Courses", icon: LiaBookSolid },
+    { label: "Calendar", path: "/Kambaz/Calendar", icon: IoCalendarOutline },
+    { label: "Inbox", path: "/Kambaz/Inbox", icon: FaInbox },
+    { label: "Labs", path: "/Labs", icon: LiaCogSolid },
+  ];
+
   return (
-    <ListGroup id="wd-kambaz-navigation" 
-      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2"
-      style={{ width: "110px" }}>
+    <ListGroup id="wd-kambaz-navigation" style={{ width: 120 }}
+      className="rounded-0 position-fixed bottom-0 top-0 d-none d-md-block bg-black z-2">
       
-      <ListGroup.Item 
-        action
-        href="https://www.northeastern.edu/"
-        target="_blank"
-        className="bg-black border-0 text-center p-2">
+      <ListGroup.Item id="wd-neu-link" target="_blank" href="https://www.northeastern.edu/"
+        action className="bg-black border-0 text-center">
         <img src="/images/NEU.png" width="75px" alt="NEU" />
       </ListGroup.Item>
 
-      <ListGroup.Item 
-        to="/Kambaz/Account" 
-        as={Link}
-        className={`text-center border-0 p-2 ${location.pathname === "/Kambaz/Account" ? "bg-white text-danger" : "bg-black text-white"}`}>
-        <FaRegCircleUser className="fs-1" style={{ color: location.pathname === "/Kambaz/Account" ? "#dc3545" : "white" }} />
-        <div className="mt-1">Account</div>
+      <ListGroup.Item as={Link} to="/Kambaz/Account" className={`text-center border-0 bg-black
+            ${pathname.includes("Account") ? "bg-white text-danger" : "bg-black text-white"}`}>
+        <FaRegCircleUser className={`fs-1 ${pathname.includes("Account") ? "text-danger" : "text-white"}`} />
+        <br />
+        Account
       </ListGroup.Item>
 
-      <ListGroup.Item 
-        to="/Kambaz/Dashboard" 
-        as={Link}
-        className={`text-center border-0 p-2 ${location.pathname === "/Kambaz/Dashboard" ? "bg-white text-danger" : "bg-black text-white"}`}>
-        <AiOutlineDashboard className="fs-1" style={{ color: "#dc3545" }} />
-        <div className="mt-1">Dashboard</div>
-      </ListGroup.Item>
-
-      <ListGroup.Item 
-        to="/Kambaz/Courses" 
-        as={Link}
-        className={`text-center border-0 p-2 ${location.pathname === "/Kambaz/Courses" ? "bg-white text-danger" : "bg-black text-white"}`}>
-        <LiaBookSolid className="fs-1" style={{ color: "#dc3545" }} />
-        <div className="mt-1">Courses</div>
-      </ListGroup.Item>
-
-      <ListGroup.Item 
-        to="/Kambaz/Calendar" 
-        as={Link}
-        className={`text-center border-0 p-2 ${location.pathname === "/Kambaz/Calendar" ? "bg-white text-danger" : "bg-black text-white"}`}>
-        <AiOutlineCalendar className="fs-1" style={{ color: "#dc3545" }} />
-        <div className="mt-1">Calendar</div>
-      </ListGroup.Item>
-
-      <ListGroup.Item 
-        to="/Kambaz/Inbox" 
-        as={Link}
-        className={`text-center border-0 p-2 ${location.pathname === "/Kambaz/Inbox" ? "bg-white text-danger" : "bg-black text-white"}`}>
-        <AiOutlineInbox className="fs-1" style={{ color: "#dc3545" }} />
-        <div className="mt-1">Inbox</div>
-      </ListGroup.Item>
-
-      <ListGroup.Item 
-        to="/Labs" 
-        as={Link}
-        className={`text-center border-0 p-2 ${location.pathname === "/Labs" ? "bg-white text-danger" : "bg-black text-white"}`}>
-        <FaFlask className="fs-1" style={{ color: "#dc3545" }} />
-        <div className="mt-1">Labs</div>
-      </ListGroup.Item>
+      {links.map((link) => (
+        <ListGroup.Item key={link.path} as={Link} to={link.path} className={`bg-black text-center border-0
+              ${pathname.includes(link.label) ? "text-danger bg-white" : "text-white bg-black"}`}>
+          {link.icon({ className: "fs-1 text-danger" })}
+          <br />
+          {link.label}
+        </ListGroup.Item>
+      ))}
     </ListGroup>
   );
 }
