c6d20a4108438efee0c25201c258ce5c9b503c6f
Hyoungseo Son <son.engr.kr@gmail.com>
2025-05-24 23:00:06 -0400

    3.8.5 Data Driven Course Navigation (On Your Own)

--- kambaz-react-web-app/src/Kambaz/Courses/Navigation.tsx
+++ kambaz-react-web-app/src/Kambaz/Courses/Navigation.tsx
@@ -1,59 +1,24 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useParams } from "react-router-dom";
 import { ListGroup } from "react-bootstrap";
 // TODO(A2): Can I use location.pathname.includes to check if the current path is active?
 export default function CourseNavigation() {
   const location = useLocation();
+  const { courseId } = useParams();
+  const links = ["Home", "Modules", "Piazza", "Zoom", "Assignments", "Quizzes", "Grades", "People"];
+
   return (
     <ListGroup id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <ListGroup.Item 
-        as={Link}
-        to="/Kambaz/Courses/1234/Home"
-        id="wd-course-home-link"
-        className={`border-0 ${location.pathname.includes("/Home") ? "active" : "text-danger"}`}>
-        Home
-      </ListGroup.Item>
-      <ListGroup.Item 
-        as={Link}
-        to="/Kambaz/Courses/1234/Modules"
-        id="wd-course-modules-link"
-        className={`border-0 ${location.pathname.includes("/Modules") ? "active" : "text-danger"}`}>
-        Modules
-      </ListGroup.Item>
-      <ListGroup.Item 
-        as={Link}
-        to="/Kambaz/Courses/1234/Piazza"
-        id="wd-course-piazza-link"
-        className={`border-0 ${location.pathname.includes("/Piazza") ? "active" : "text-danger"}`}>
-        Piazza
-      </ListGroup.Item>
-      <ListGroup.Item 
-        as={Link}
-        to="/Kambaz/Courses/1234/Zoom"
-        id="wd-course-zoom-link"
-        className={`border-0 ${location.pathname.includes("/Zoom") ? "active" : "text-danger"}`}>
-        Zoom
-      </ListGroup.Item>
-      <ListGroup.Item 
-        as={Link}
-        to="/Kambaz/Courses/1234/Assignments"
-        id="wd-course-quizzes-link"
-        className={`border-0 ${location.pathname.includes("/Assignments") ? "active" : "text-danger"}`}>
-        Assignments
-      </ListGroup.Item>
-      <ListGroup.Item 
-        as={Link}
-        to="/Kambaz/Courses/1234/Quizzes"
-        id="wd-course-assignments-link"
-        className={`border-0 ${location.pathname.includes("/Quizzes") ? "active" : "text-danger"}`}>
-        Quizzes
-      </ListGroup.Item>
-      <ListGroup.Item 
-        as={Link}
-        to="/Kambaz/Courses/1234/People"
-        id="wd-course-people-link"
-        className={`border-0 ${location.pathname.includes("/People") ? "active" : "text-danger"}`}>
-        People
-      </ListGroup.Item>
+      {links.map((link) => (
+        <ListGroup.Item
+          key={link}
+          as={Link}
+          to={`/Kambaz/Courses/${courseId}/${link}`}
+          id={`wd-course-${link.toLowerCase()}-link`}
+          className={`border-0 ${location.pathname.includes(`/${link}`) ? "active" : "text-danger"}`}
+        >
+          {link}
+        </ListGroup.Item>
+      ))}
     </ListGroup>
   );
 }
