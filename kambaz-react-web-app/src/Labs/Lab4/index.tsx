ca88b2460f42bc2c445a251c3ae665dcf96f5c53
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-02 03:35:01 -0400

    4.2.2.5 Date State Variables

--- kambaz-react-web-app/src/Labs/Lab4/index.tsx
+++ kambaz-react-web-app/src/Labs/Lab4/index.tsx
@@ -5,6 +5,7 @@ import EventObject from "./EventObject";
 import Counter from "./Counter";
 import BooleanStateVariables from "./BooleanStateVariables";
 import StringStateVariables from "./StringStateVariables";
+import DateStateVariable from "./DateStateVariable";
 
 export default function Lab4() {
   function sayHello() {
@@ -21,6 +22,7 @@ export default function Lab4() {
       <Counter />
       <BooleanStateVariables />
       <StringStateVariables />
+      <DateStateVariable />
     </div>
   );
 } 
