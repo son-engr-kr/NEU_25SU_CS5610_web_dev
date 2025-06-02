f2f6c6d476780b851cce440a21cee1dec811910e
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-02 03:20:36 -0400

    4.2.1.1 Handling Click Events; 4.2.1.2 Passing Data when Handling Events;

--- kambaz-react-web-app/src/Labs/Lab4/index.tsx
+++ kambaz-react-web-app/src/Labs/Lab4/index.tsx
@@ -1,8 +1,11 @@
+import ClickEvent from "./ClickEvent";
+import PassingDataOnEvent from "./PassingDataOnEvent";
 export default function Lab4() {
   return (
     <div id="wd-lab4">
       <h2>Lab 4</h2>
-      
+      <ClickEvent />
+      <PassingDataOnEvent />
     </div>
   );
 } 
