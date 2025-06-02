f4db330f517bc8b1eae1565f6038b385aa46ae55
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-02 04:28:29 -0400

    4.2.2.8 Sharing State Between Components

--- kambaz-react-web-app/src/Labs/Lab4/index.tsx
+++ kambaz-react-web-app/src/Labs/Lab4/index.tsx
@@ -8,6 +8,7 @@ import StringStateVariables from "./StringStateVariables";
 import DateStateVariable from "./DateStateVariable";
 import ObjectStateVariable from "./ObjectStateVariable";
 import ArrayStateVariable from "./ArrayStateVariable";
+import ParentStateComponent from "./ParentStateComponent";
 
 export default function Lab4() {
   function sayHello() {
@@ -27,6 +28,7 @@ export default function Lab4() {
       <DateStateVariable />
       <ObjectStateVariable />
       <ArrayStateVariable />
+      <ParentStateComponent />
     </div>
   );
 } 
