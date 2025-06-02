63fefdafadedfcc9d1e19c6fa82e41a0f760f0dc
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-02 04:48:04 -0400

    4.3.2 Create a Hello World Redux component

--- kambaz-react-web-app/src/Labs/index.tsx
+++ kambaz-react-web-app/src/Labs/index.tsx
@@ -4,23 +4,29 @@ import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
+import store from "./store";
+import { Provider } from "react-redux";
+
 export default function Labs() {
   return (
-    <div>
-      <h1>Labs</h1>
-      <div>
-        <p>Name: Hyoungseo Son (son.hyo@northeastern.edu)</p>
-        <p>Section: CS5610 SU25 41980</p>
-        <p>GitHub: <a href="https://github.com/son-engr-kr/NEU_25SU_CS5610_web_dev/tree/main/kambaz-react-web-app" target="_blank">NEU_25SU_CS5610_web_dev/kambaz-react-web-app</a></p>
+    <Provider store={store}>
+      <div className="container-fluid">
+        <h1>Labs</h1>
+        <div>
+          <p>Name: Hyoungseo Son (son.hyo@northeastern.edu)</p>
+          <p>Section: CS5610 SU25 41980</p>
+          <p>GitHub: <a href="https://github.com/son-engr-kr/NEU_25SU_CS5610_web_dev/tree/main/kambaz-react-web-app" target="_blank">NEU_25SU_CS5610_web_dev/kambaz-react-web-app</a></p>
+        </div>
+        <TOC />
+        { /* Routes change the contents based on the URL */ }
+        <Routes>
+          <Route path="/" element={<Navigate to="Lab1" />} />
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2/*" element={<Lab2 />} />
+          <Route path="Lab3/*" element={<Lab3 />} />
+          <Route path="Lab4/*" element={<Lab4 />} />
+        </Routes>
       </div>
-      <TOC />
-      { /* Routes change the contents based on the URL */ }
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2/*" element={<Lab2 />} />
-        <Route path="Lab3/*" element={<Lab3 />} />
-        <Route path="Lab4/*" element={<Lab4 />} />
-      </Routes>
-    </div>
-);}
+    </Provider>
+  );
+}
