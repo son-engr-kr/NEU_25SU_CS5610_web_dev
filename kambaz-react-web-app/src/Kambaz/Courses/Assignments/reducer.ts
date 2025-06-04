94f904cbacfaa42ad6a46d3c605f1edb190f92bb
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-03 22:23:04 -0400

    4.4.5.1 Assignments Reducer

---
+++ kambaz-react-web-app/src/Kambaz/Courses/Assignments/reducer.ts
@@ -0,0 +1,38 @@
+import { createSlice } from "@reduxjs/toolkit";
+import * as db from "../../Database";
+
+const initialState = {
+  assignments: db.assignments,
+};
+
+const assignmentsSlice = createSlice({
+  name: "assignments",
+  initialState,
+  reducers: {
+    addAssignment: (state, { payload: assignment }) => {
+      const newAssignment = {
+        _id: new Date().getTime().toString(),
+        ...assignment,
+      };
+      state.assignments = [...state.assignments, newAssignment] as any;
+    },
+    deleteAssignment: (state, { payload: assignmentId }) => {
+      state.assignments = state.assignments.filter(
+        (a: any) => a._id !== assignmentId);
+    },
+    updateAssignment: (state, { payload: assignment }) => {
+      state.assignments = state.assignments.map((a: any) =>
+        a._id === assignment._id ? assignment : a
+      ) as any;
+    },
+    editAssignment: (state, { payload: assignmentId }) => {
+      state.assignments = state.assignments.map((a: any) =>
+        a._id === assignmentId ? { ...a, editing: true } : a
+      ) as any;
+    },
+  },
+});
+
+export const { addAssignment, deleteAssignment, updateAssignment, editAssignment } =
+  assignmentsSlice.actions;
+export default assignmentsSlice.reducer; 
