e8846dea381e97e83f7562392a83ec85139991eb
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-06 18:38:12 -0400

    5.2.3.2 Retrieving Object Properties from a Server

--- kambaz-node-server-app/Lab5/WorkingWithObjects.js
+++ kambaz-node-server-app/Lab5/WorkingWithObjects.js
@@ -5,6 +5,10 @@ const assignment = {
 };
 export default function WorkingWithObjects(app) {
     app.get("/lab5/assignment", (req, res) => {
-        res.json(assignment);
+      res.json(assignment);
     });
-};
+    app.get("/lab5/assignment/title", (req, res) => {
+      res.json(assignment.title);
+    });
+  }
+  
