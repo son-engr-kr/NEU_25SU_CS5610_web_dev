7fa29b108d62bea578b02205feba4fb914f126ef
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-06 15:15:20 -0400

    5.2 Lab Exercises

--- kambaz-react-web-app/src/Labs/TOC.tsx
+++ kambaz-react-web-app/src/Labs/TOC.tsx
@@ -18,6 +18,8 @@ export default function TOC() {
         active={pathname.includes("Lab3")}> Lab 3 </Nav.Link> </Nav.Item>
       <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab4" id="wd-a4"
         active={pathname.includes("Lab4")}> Lab 4 </Nav.Link> </Nav.Item>
+      <Nav.Item> <Nav.Link as={Link} to="/Labs/Lab5" id="wd-a5"
+        active={pathname.includes("Lab5")}> Lab 5 </Nav.Link> </Nav.Item>
       <Nav.Item>
         <Nav.Link to="/Kambaz" as={Link}>Kambaz</Nav.Link>
       </Nav.Item>
