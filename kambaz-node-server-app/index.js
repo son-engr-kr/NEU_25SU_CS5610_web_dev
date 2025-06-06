f2a2ba4d419cda50d185a9a2eb29752ce6bbd6d7
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-06 14:40:30 -0400

    5.1.7 Configuring Node.js to use ES6

--- kambaz-node-server-app/index.js
+++ kambaz-node-server-app/index.js
@@ -1,6 +1,5 @@
-const express = require('express')
-const app = express()
+import express from 'express';
+const app = express();
 app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {
-  res.send('Welcome to Full Stack Development!')})
-app.listen(4000)
+app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.listen(4000);
