55099ea318ff611f59aa736564c87b6a7c7f4296
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-06 16:41:58 -0400

    5.2.2.3 On Your Own

--- kambaz-react-web-app/src/Labs/Lab5/PathParameters.tsx
+++ kambaz-react-web-app/src/Labs/Lab5/PathParameters.tsx
@@ -11,14 +11,24 @@ export default function PathParameters() {
              onChange={(e) => setA(e.target.value)}/>
       <FormControl className="mb-2" id="wd-path-parameter-b" type="number" defaultValue={b}
              onChange={(e) => setB(e.target.value)}/>
-      <a className="btn btn-primary me-2" id="wd-path-parameter-add"
-         href={`${REMOTE_SERVER}/lab5/add/${a}/${b}`}>
-         Add {a} + {b}
-      </a>
-      <a className="btn btn-danger" id="wd-path-parameter-subtract" 
-         href={`${REMOTE_SERVER}/lab5/subtract/${a}/${b}`}>
-         Substract {a} - {b}
-      </a>
+      <div className="d-flex flex-wrap gap-2">
+        <a className="btn btn-primary" id="wd-path-parameter-add"
+           href={`${REMOTE_SERVER}/lab5/add/${a}/${b}`}>
+           Add {a} + {b}
+        </a>
+        <a className="btn btn-danger" id="wd-path-parameter-subtract" 
+           href={`${REMOTE_SERVER}/lab5/subtract/${a}/${b}`}>
+           Subtract {a} - {b}
+        </a>
+        <a className="btn btn-success" id="wd-path-parameter-multiply"
+           href={`${REMOTE_SERVER}/lab5/multiply/${a}/${b}`}>
+           Multiply {a} * {b}
+        </a>
+        <a className="btn btn-warning" id="wd-path-parameter-divide"
+           href={`${REMOTE_SERVER}/lab5/divide/${a}/${b}`}>
+           Divide {a} / {b}
+        </a>
+      </div>
       <hr />
     </div>
   );
