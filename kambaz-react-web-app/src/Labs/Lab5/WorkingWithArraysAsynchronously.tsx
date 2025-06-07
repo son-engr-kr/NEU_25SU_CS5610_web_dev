ea3bb6f359340c25185557080e04f94f69db0cae
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-06 20:02:11 -0400

    5.2.5.7 Deleting Data from a Server Asynchronously

--- kambaz-react-web-app/src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
+++ kambaz-react-web-app/src/Labs/Lab5/WorkingWithArraysAsynchronously.tsx
@@ -1,19 +1,25 @@
 import { useState, useEffect } from "react";
 import { ListGroup } from "react-bootstrap";
+import { FaTrash } from "react-icons/fa"; // Importing the trash icon
 import * as client from "./client";
 
 export default function WorkingWithArraysAsynchronously() {
   const [todos, setTodos] = useState<any[]>([]);
-  
+
   const fetchTodos = async () => {
     const todos = await client.fetchTodos();
     setTodos(todos);
   };
-  
+
+  const removeTodo = async (todo: any) => {
+    const updatedTodos = await client.removeTodo(todo);
+    setTodos(updatedTodos);
+  };
+
   useEffect(() => {
     fetchTodos();
   }, []);
-  
+
   return (
     <div id="wd-asynchronous-arrays">
       <h3>Working with Arrays Asynchronously</h3>
@@ -21,6 +27,8 @@ export default function WorkingWithArraysAsynchronously() {
       <ListGroup>
         {todos.map((todo) => (
           <ListGroup.Item key={todo.id}>
+            <FaTrash onClick={() => removeTodo(todo)}
+                     className="text-danger float-end mt-1" id="wd-remove-todo"/>
             <input type="checkbox" className="form-check-input me-2"
                    defaultChecked={todo.completed}/>
             <span style={{ textDecoration: todo.completed ? "line-through" : "none" }}>
@@ -28,7 +36,7 @@ export default function WorkingWithArraysAsynchronously() {
             </span>
           </ListGroup.Item>
         ))}
-      </ListGroup> 
+      </ListGroup>
       <hr />
     </div>
   );
--- kambaz-react-web-app/src/Labs/Lab5/client.ts
+++ kambaz-react-web-app/src/Labs/Lab5/client.ts
@@ -21,3 +21,10 @@ export const fetchTodos = async () => {
   const response = await axios.get(TODOS_API);
   return response.data;
 };
+
+
+export const removeTodo = async (todo: any) => {
+    const response = await axios.get(`${TODOS_API}/${todo.id}/delete`);
+    return response.data;
+  };
+  
