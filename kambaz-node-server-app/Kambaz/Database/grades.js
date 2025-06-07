903ae1666a776cfa2d2e8ff0310a35772ddcc3ad
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-06 20:31:05 -0400

    5.3.1 Migrating the "Database" to the Server

---
+++ kambaz-node-server-app/Kambaz/Database/grades.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        "_id": "1",
+        "student": "123",
+        "assignment": "A101",
+        "grade": "89"
+    },
+    {
+        "_id": "2",
+        "student": "234",
+        "assignment": "A101",
+        "grade": "92"
+    },
+    {
+        "_id": "3",
+        "student": "345",
+        "assignment": "A101",
+        "grade": "78"
+    },
+    {
+        "_id": "4",
+        "student": "456",
+        "assignment": "A101",
+        "grade": "85"
+    },
+    {
+        "_id": "5",
+        "student": "567",
+        "assignment": "A101",
+        "grade": "94"
+    },
+    {
+        "_id": "6",
+        "student": "234",
+        "assignment": "A201",
+        "grade": "88"
+    },
+    {
+        "_id": "7",
+        "student": "789",
+        "assignment": "A201",
+        "grade": "91"
+    },
+    {
+        "_id": "8",
+        "student": "890",
+        "assignment": "A201",
+        "grade": "76"
+    },
+    {
+        "_id": "9",
+        "student": "123",
+        "assignment": "A201",
+        "grade": "95"
+    }
+]; 
