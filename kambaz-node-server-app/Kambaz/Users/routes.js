ce9a519759024325713614f0d0269eaafbf29007
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-18 03:13:37 -0400

    6.2.6.6 Updating a Document in MongoDB with Mongoose

--- kambaz-node-server-app/Kambaz/Users/routes.js
+++ kambaz-node-server-app/Kambaz/Users/routes.js
@@ -58,12 +58,15 @@ export default function UserRoutes(app) {
     res.json(enrolledUsers);
   };
 
-  const updateUser = (req, res) => {
+  const updateUser = async (req, res) => {
     const userId = req.params.userId;
     const userUpdates = req.body;
-    dao.updateUser(userId, userUpdates);
-    const currentUser = dao.findUserById(userId);
-    req.session["currentUser"] = currentUser;
+    await dao.updateUser(userId, userUpdates);
+    const currentUser = req.session["currentUser"];
+    if (currentUser && currentUser._id === userId) {
+      req.session["currentUser"] = { ...currentUser, ...userUpdates };
+    }
+ 
     res.json(currentUser);
   };
 
