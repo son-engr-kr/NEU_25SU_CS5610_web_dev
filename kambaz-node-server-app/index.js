05b476b895b44f141ff322c41b25a5238de2cb59
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-17 22:55:59 -0400

    6.2.2 Configuring Connection Strings as Environment Variables

--- kambaz-node-server-app/index.js
+++ kambaz-node-server-app/index.js
@@ -10,7 +10,7 @@ import session from "express-session";
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 import mongoose from "mongoose";
 
-const CONNECTION_STRING = "mongodb://127.0.0.1:27017/kambaz"
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz"
 mongoose.connect(CONNECTION_STRING);
 const app = express();
 
