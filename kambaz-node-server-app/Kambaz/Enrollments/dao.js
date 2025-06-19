02a8cae869d4ff8ed4ef58cfcc0aeb55031e72d3
Hyoungseo Son <son.engr.kr@gmail.com>
2025-06-19 07:29:06 -0400

    6.4.3.1 Declaring Enrollments as a Many to Many Relationship

--- kambaz-node-server-app/Kambaz/Enrollments/dao.js
+++ kambaz-node-server-app/Kambaz/Enrollments/dao.js
@@ -1,31 +1,15 @@
-import Database from "../Database/index.js";
-import { v4 as uuidv4 } from "uuid";
-
-export function enrollUserInCourse(userId, courseId) {
-  const { enrollments } = Database;
-  // Check if already enrolled
-  const existingEnrollment = enrollments.find(
-    (enrollment) => enrollment.user === userId && enrollment.course === courseId
-  );
-  
-  if (!existingEnrollment) {
-    enrollments.push({ _id: uuidv4(), user: userId, course: courseId });
-  }
+import model from "./model.js";
+export async function findCoursesForUser(userId) {
+ const enrollments = await model.find({ user: userId }).populate("course");
+ return enrollments.map((enrollment) => enrollment.course);
 }
-
-export function unenrollUserFromCourse(userId, courseId) {
-  const { enrollments } = Database;
-  Database.enrollments = enrollments.filter(
-    (enrollment) => !(enrollment.user === userId && enrollment.course === courseId)
-  );
+export async function findUsersForCourse(courseId) {
+ const enrollments = await model.find({ course: courseId }).populate("user");
+ return enrollments.map((enrollment) => enrollment.user);
 }
-
-export function findEnrollmentsByUserId(userId) {
-  const { enrollments } = Database;
-  return enrollments.filter((enrollment) => enrollment.user === userId);
+export function enrollUserInCourse(user, course) {
+ return model.create({ user, course, _id: `${user}-${course}` });
 }
-
-export function findAllEnrollments() {
-  const { enrollments } = Database;
-  return enrollments;
+export function unenrollUserFromCourse(user, course) {
+ return model.deleteOne({ user, course });
 }
